a598d9f3c526e6d1bfbb7e99566a2f339df34e00
zzahra12 <deswitaay22@gmail.com>
2025-02-10 12:50:26 +0700

    ab

---
+++ projek2/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <meta name="viewport" content="width=device-width, initial-
+scale=1.0">
+      <title>Mengenal Form </title>
+</head>
+
+<body>
+      <form action="../proses.php" method="POST">
+            <fieldset>
+                  <legend>
+                        <h1>Formulir Peserta </h1>
+                  </legend>
+                  <p>Silahkan isi data berikut :</p>
+                  NIS : <input type="number" name="nisn">
+                  </p>
+                  <p>
+
+                        Nama Lengkap : <input type="text" name="nama">
+                  </p>
+                  <p>
+
+                        Tempat Lahir : <input type="text" name="tempat_lahir">
+                  </p>
+                  <p>
+
+                        Tanggal Lahir : <input type="date" name="tanggal_lahir">
+                  </p>
+                  <p>
+                        Jenis Kelamin :
+                        <input type="radio" name="jenkel" value="laki-laki">Laki-laki
+                        <input type="radio" name="jenkel" value="perempuan">Perempuan
+                  </p>
+                  <p>
+
+                        Agama : <select id="Agama" name="agama">
+                              <option value="Islam">Islam</option>
+                              <option value="Kristen">Kristen</option>
+                              <option value="Katolik">Katolik</option>
+                              <option value="Hindu">Hindu</option>
+                              <option value="Budha">Budha</option>
+                              <option value="Konghucu">Konghucu</option>
+                        </select>
+                  </p>
+                  <p>
+
+                  <p>ALAMAT :</p>
+                  <textarea name="alamat" id="Alamat" cols="30" rows="10"></textarea>
+                  </p>
+                  <p>
+                        Asal Sekolah : <input type="text" name="asal_sekolah">
+                  </p>
+                  <p>
+                        Pilih Jurusan 1 : <select id="Pil Jurusan 1" name="select1">
+                              <option value="TIDAK MEMILIH">TIDAK MEMILIH</option>
+                              <option value="pplg">PENGEMBANGAN PERANGKAT LUNAK & GIM
+                              </option>
+                              <option value="tjkt">TEKNIK JARINGAN KOMPUTER &
+                                    TELEKOMUNIKASI</option>
+                              <option value="dkv">DESAIN KOMUNIKASI VISUAL</option>
+                              <option value="sp">SENI PERTUNJUKAN</option>
+                              <option value="akl">AKUNTANSI KEUANGAN LEMBAGA</option>
+                              <option value="mplb">MANAJEMEN PERKANTORAN & LAYANAN
+                                    BISNIS</option>
+                              <option value="ph">PERHOTELAN</option>
+                              <option value="pm">PEMASARAN</option>
+                              <option value="bdp">BROADCASTING & PERFILMAN</option>
+                        </select>
+                  </p>
+                  <p>
+
+                        Pilih Jurusan 2 : <select id="Pil Jurusan 2" name="select2">
+                              <option value="TIDAK MEMILIH">TIDAK MEMILIH</option>
+                              <option value="pplg">PENGEMBANGAN PERANGKAT LUNAK & GIM
+                              </option>
+                              <option value="tjkt">TEKNIK JARINGAN KOMPUTER &
+                                    TELEKOMUNIKASI</option>
+                              <option value="dkv">DESAIN KOMUNIKASI VISUAL</option>
+                              <option value="sp">SENI PERTUNJUKAN</option>
+                              <option value="akl">AKUNTANSI KEUANGAN LEMBAGA</option>
+                              <option value="mplb">MANAJEMEN PERKANTORAN & LAYANAN
+                                    BISNIS</option>
+                              <option value="ph">PERHOTELAN</option>
+                              <option value="pm">PEMASARAN</option>
+                              <option value="bdp">BROADCASTING & PERFILMAN</option>
+                        </select>
+                  </p>
+                  <p>
+
+                        Ekstrakulikuler:
+                        <input type="checkbox" name="ekskul[]" value="osis">OSIS
+                        <input type="checkbox" name="ekskul[]" value="DA">PRAMUKA
+                        <input type="checkbox" name="ekskul[]" value="pecinta alam">PA
+                        <input type="checkbox" name="ekskul[]" value="remas">REMAS AL-KAUTSAR
+                        <input type="checkbox" name="ekskul[]" value="palang merah remaja">PMR/UKS
+                        <input type="checkbox" name="ekskul[]" value="futsal">FUTSAL
+                        <input type="checkbox" name="ekskul[]" value="basket">BASKET
+                        <input type="checkbox" name="ekskul[]" value="voli">VOLI
+                        <input type="checkbox" name="ekskul[]" value="teater">TEATER KUSUMA
+                        <input type="checkbox" name="ekskul[]" value="laskar">LA
+                        <input type="checkbox" name="ekskul[]" value="gerakan disiplin sekolah">GDS
+                  </p>
+                  <p>
+                        <button type="submit" name="datasiswa"> Submit </button>
+                  </p>
+            </fieldset>
+      </form>
+</body>
+
+</html>
